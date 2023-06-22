4594ea5148cd27c71ecbc773ac71a3dc72aa878a
Gustavo Machado <gmachado@jazida.com>
2023-06-22 20:36:34 -0300

    arquivos de correcao

--- src/tests/nota.test.js
+++ src/tests/nota.test.js
@@ -1,35 +1,44 @@
 const Nota = require('../models/nota');
 
 describe('Calculo da média final', () => {
-  test('a media é zero se não tem notas', () => {
-    let nota = new Nota(null, 0, 0, 0);
-    expect(nota.mediaFinal()).toEqual(0);
-    nota = new Nota(null, null, null, null)
-    expect(nota.mediaFinal()).toEqual(0);
+  it('Deve calcular a média final', async () => {
+    const notas = new Nota('Disciplina', 5.2, 3.8, 4.8);
+    expect(notas.mediaFinal()).toBe(4.96);
+    expect(notas.mediaCA()).toBe('MI');
   });
 
-  test('a média é (0.4 * a1) + (0.6 * a2) se tem apenas a1 e a2', () => {
-    let nota = new Nota(null, 3, 5, null);
-    expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-    nota = new Nota(null, 7, 4, null);
-    expect(nota.mediaFinal()).toEqual(0.4 * 7 + 0.6 * 4);
-  });
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 6.4, 4.2, 0);
+    expect(notas.mediaCA()).toBe('MM');
+  })
 
-  test('a média é (0.4 * a3) + (0.6 * a2) se a3 substitui a1', () => {
-    let nota = new Nota(null, 0, 5, 3);
-    expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-    nota = new Nota(null, 2, 5, 3);
-    expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-    nota = new Nota(null, 2, 5, 6);
-    expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-  });
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 5.2, 3.8, 4.96);
+    expect(notas.mediaFinal()).toBe(5.056);
+    expect(notas.mediaCA()).toBe('MM');
+  })
 
-  test('a média é (0.4 * a1) + (0.6 * a3) se a3 substitui a2', () => {
-    let nota = new Nota(null, 6, 0, 5);
-    expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-    nota = new Nota(null, 6, 4, 5);
-    expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-    nota = new Nota(null, 6, 4, 7);
-    expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 7);
-  });
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 1.5, 6.5, 3);
+    expect(notas.mediaFinal()).toBe(5.1);
+    expect(notas.mediaCA()).toBe('MM');
+  })
+
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 0, 0, 0);
+    expect(notas.mediaFinal()).toBe(0);
+    expect(notas.mediaCA()).toBe('SR');
+  })
+
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 0, 0, 1);
+    expect(notas.mediaFinal()).toBe(0.6);
+    expect(notas.mediaCA()).toBe('II');
+  })
+
+  it('Deve calcular a média CA', async () => {
+    const notas = new Nota('Disciplina', 0, 0, 3);
+    expect(notas.mediaFinal()).toBe(1.8);
+    expect(notas.mediaCA()).toBe('MI');
+  })
 });
